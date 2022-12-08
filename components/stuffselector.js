8c175d54096f8633023cd3385eaac242e721c2bc
baanwara papiha <baanwarapapiha@gmail.com>
2022-12-08 15:50:09 +0500

    routing back and forth good

--- components/stuffselector.js
+++ components/stuffselector.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { View, Text, Button } from "react-native";
 
-const StuffSelector = ({navigation}) => {
+const StuffSelector = ({route, navigation}) => {
+    const { animal } = route.params;
     return (
         <View>
             <Text>
@@ -10,14 +11,17 @@ const StuffSelector = ({navigation}) => {
 
             <Button
             onPress={() => {
-                alert(JSON.stringify(navigation))
-                // navigation.navigate('Home', );        
+                // alert(JSON.stringify(navigation))
+                navigation.navigate('Details');        
             }}
             title="Go"
             color="#841584"
             accessibilityLabel="Go to sibling"
             />
 
+            <Text>Details of Animal / birds</Text>
+            <Text>This will contain a form of age, weigh and production</Text>
+            <Text>Animal: {JSON.stringify(animal)}</Text>
             <Button
                 title="Go to Home"
                 onPress={() => navigation.goBack()}
--- pages/detail.js
+++ pages/detail.js
@@ -2,17 +2,13 @@ import React from "react";
 import { View, Text, Button} from "react-native"
 import PagerView from 'react-native-pager-view';
 
-function DetailsScreen({ route, navigation }) {
-    // const { animal } = route.params;
-    // The above statement will cause an error, if this page is opened from settings screen
-    // because from settings screeen, currently, it has no route input
-    const animal = 'handle route.params from settings';
+function DetailsScreen({ navigation }) {
 
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Details of Animal / birds</Text>
-        <Text>This will contain a form of age, weigh and production</Text>
-        <Text>Animal: {JSON.stringify(animal)}</Text>
+        <Text>Details of Formulated Feed</Text>
+        {/* <Text>This will contain a form of age, weigh and production</Text>
+        <Text>Animal: {JSON.stringify(animal)}</Text> */}
         <Button
             title="Go to Home"
             onPress={() => navigation.goBack()}
