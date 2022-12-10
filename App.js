5f7aee955b4a5e6f3375a09221b38b6336fd23e6
baanwara papiha <baanwarapapiha@gmail.com>
2022-12-10 20:45:58 +0500

    paused the work

--- App.js
+++ App.js
@@ -2,34 +2,38 @@ import React, { useState } from "react";
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from "./pages/home";
-import DetailsScreen from "./pages/detail";
 import Settings from "./pages/settings";
 import Landing from './pages/landing';
+// redux
+import store from './app/store'
+import { Provider } from 'react-redux'
 
 const Tab = createBottomTabNavigator();
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Tab.Navigator initialRouteName="Home"
-        screenOptions={{
-          headerStyle: {
-            backgroundColor: '#f4511e',
-          },
-          headerTintColor: '#fff',
-          headerTitleStyle: {
-            fontWeight: 'bold',
-          },
-        }}      
-      >
-        {/* load this app first, then if every thing goes well load, other pages below */}
-        <Tab.Screen name="Landing" component={Landing} />
+    <Provider store={store}>
+      <NavigationContainer>
+        <Tab.Navigator initialRouteName="Home"
+          screenOptions={{
+            headerStyle: {
+              backgroundColor: '#f4511e',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+            },
+          }}      
+        >
+            {/* load this app first, then if every thing goes well load, other pages below */}
+            <Tab.Screen name="Landing" component={Landing} />
+            <Tab.Screen name="Home" component={Home} options={{ title: 'Feed Formulation'}}/>
+            <Tab.Screen name="Settings" component={Settings}/>
+        </Tab.Navigator>
+      </NavigationContainer>
 
-        {/* <Tab.Screen name="Details" component={DetailsScreen}/> */}
-        <Tab.Screen name="Home" component={Home} options={{ title: 'Feed Formulation'}}/>
-        <Tab.Screen name="Settings" component={Settings}/>
-      </Tab.Navigator>
-    </NavigationContainer>
+
+    </Provider>
   );
 }
 
