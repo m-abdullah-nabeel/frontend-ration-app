0e476ff4f36a063cda9847243711640ffc0c6d01
m-abdullah-nabeel <muhammadabdullahnabeel@gmail.com>
2022-12-19 15:35:40 +0500

    completed menu page

--- pages/menu.js
+++ pages/menu.js
@@ -1,67 +1,46 @@
 import React from "react";
-import { View, Text, FlatList, StyleSheet } from 'react-native';
+import { View, Text } from 'react-native';
 // import Item from "./animalItem";
 import { TouchableOpacity } from "react-native"
 
 const MenuScreen = ({ navigation }) => {
     return (
-        <View>
-            <View style={styles.row}>
-                <TouchableOpacity style={styles.animal}
+        <View style={{ flex: 1 }}>
+            <View style={{ flex: 1, paddingBottom: 10 }} >
+                <TouchableOpacity
                     onPress={() => {
                         navigation.navigate('Animal Selector');
                     }}
                 >
-                    <View>
-                        {/* <Text>{'image of animal'}</Text> */}
-                        {/* <Text>{'name'}</Text> */}
-                        <Text>name</Text>
+                    <View style={{
+                        backgroundColor: 'rgb(110, 10, 10)', height: '90%', borderRadius: 50,
+                        alignContent: "center", justifyContent: 'center',
+                    }}>
+                        <Text style={{
+                            fontSize: 32, fontWeight: 'bold',
+                            paddingTop: 20, paddingBottom: 20, paddingLeft: 15, paddingRight: 15,
+                            color: 'white'
+                        }}>Formulate your feed</Text>
                     </View>
                 </TouchableOpacity>
-
-                <View style={[styles.animal, styles.disabled, { flex: 3 }]}>
-                    <Text>{'name'}</Text>
+            </View >
+            <View style={{ flex: 3, flexDirection: 'row', justifyContent: 'space-around' }}>
+                <View style={{
+                    width: 100, backgroundColor: 'rgb(100, 10, 10)', color: 'white', borderRadius: 50,
+                    height: 100, alignItems: 'center', justifyContent: 'center', margin: 5, opacity: .6
+                }}>
+                    <Text style={{ color: 'white' }}>Summer Formula</Text>
                 </View>
-
+                <View style={{
+                    width: 100, backgroundColor: 'rgb(100, 10, 10)', color: 'white', borderRadius: 50,
+                    height: 100, alignItems: 'center', justifyContent: 'center', margin: 5, opacity: .6
+                }}>
+                    <Text style={{ color: 'white' }}>Winter Formula</Text>
+                </View>
             </View>
 
-        </View>
+        </View >
     )
 }
-
-const styles = StyleSheet.create({
-    header: {
-        fontWeight: '900',
-        fontSize: 32,
-        padding: 20,
-        // width: "60%",
-        backgroundColor: "beige",
-        borderWidth: 5,
-    },
-    animal: {
-        backgroundColor: "rgba(195, 50, 250, .5)",
-        borderRadius: 20,
-        padding: 20,
-        margin: 10,
-        borderColor: 'black',
-        flex: 1,
-        flexDirection: "row",
-        justifyContent: "space-between",
-        alignItems: "center",
-        width: '50%',
-        height: 150
-
-    },
-    disabled: {
-        opacity: .5
-    },
-    row: {
-        flex: 1,
-        flexDirection: 'row',
-        width: '100%',
-        flexWrap: 'wrap'
-    }
-});
-
 
 export default MenuScreen;
