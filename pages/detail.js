a17a9990142a3261ae12078778e4bc76257bd57a
m-abdullah-nabeel <muhammadabdullahnabeel@gmail.com>
2023-03-30 17:10:59 +0500

    Changes to show only to Dr. Mubarak saved

--- pages/detail.js
+++ pages/detail.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useCallback } from "react";
 import { View, Text, Button, TouchableOpacity, Linking, Image, ScrollView, Pressable } from "react-native"
 import nutrientdata from '../assets/data/feeds_nutrient.json';
 import animalsReqdata from '../assets/data/nutrients_required.json';
+import { Dimensions } from 'react-native';
 
 import RequirementsSheep from '../assets/data/animal_requirements/sheep.json';
 import RequirementsGoat from '../assets/data/animal_requirements/goat.json';
@@ -34,55 +35,69 @@ const ResultCheck = (props) => {
   }
 
   if (res['available']) {
+    // correct results
     if (res['status'] == 0) {
-      // correct results
       return (
         <View>
+          {/* red block */}
           <View style={{
-            backgroundColor: "rgb(110, 30, 1)",
-            width: "100%",
-            borderRadius: 10,
-            padding: 20
+            backgroundColor: "rgba(153, 10, 10, 0.8)",
+            borderRadius: 10, padding: 10, margin: 5, alignSelf: "center", width: "100%",
           }}>
             <Text style={{
-              fontSize: 18, fontWeight: 'bold', color: "#fff", alignSelf: "center",
-              // marginTop: 5, marginLeft: 20, marginRight: 20, marginBottom: 2,
+              fontSize: 18, fontWeight: 'bold', color: "rgb(250, 250, 250)", alignSelf: "center",
             }}>
               Least Cost Feed Formulation
             </Text>
-            <Text style={{
-              color: "#fff", fontSize: 12,
-              marginBottom: 5, marginLeft: 20, marginRight: 10, alignSelf: "center"
-            }}>
+            <Text style={{ color: "rgb(250, 250, 250)", fontSize: 12, alignSelf: "center" }}>
               {compo.length} {t("feedstuffs")}
             </Text>
           </View>
 
-          <View style={{
-            backgroundColor: "rgb(30, 130, 30)", borderRadius: 10, padding: 10, marginTop: 1, marginBottom: 10
-          }}>
-            <Text style={{ fontSize: 24, color: "#fff", fontWeight: 'bold', alignSelf: "center" }}>
+          {/* green block */}
+          <View style={{ backgroundColor: "rgba(10, 120, 10, 0.9)", borderRadius: 10, padding: 10, margin: 5, alignSelf: "center", width: "100%" }}>
+            <Text style={{ fontSize: 24, color: "#fff", fontWeight: 'bold', alignSelf: "center", padding: 10 }}>
               {t("feed composition line")}
             </Text>
-
           </View>
 
-          <View style={{
-            // backgroundColor: "pink",
-            padding: 10, borderColor: 'green', borderWidth: 3, borderRadius: 10
-          }}>
+          {/* dry matter formula block */}
+          <View style={{ backgroundColor: "orange", opacity: 0.7, padding: 10, borderColor: 'green', borderWidth: 3, borderRadius: 10 }}>
+            {/* intro line */}
             <Text style={{ fontWeight: 'bold', fontSize: 22, borderBottomColor: 'black', borderBottomWidth: 4 }}>
               {t("Dry Matter Formula")}
             </Text>
 
+            {console.log("==========>\n==========>\n==========>\n==========>\n==========>\n==========>\n")}
+            {console.log("Starting Debugging for incorrect as fed results")}
             {
-              Object.keys(res['results']).map(k => {
+              console.log("Results (DM Basis): " + JSON.stringify(res['results']))
+            }
+            {
+              Object.keys(res['results']).map((k, y) => {
                 curr_ = nutrientdata.find(x => x.name == k)
                 percent_val = Math.round(res['results'][k] * 100)
-                dm_a.push(curr_['DM%'] * percent_val)
-                cp_a.push(curr_['CP'] * percent_val)
-                me_a.push(curr_['ME'] * percent_val)
-                ndf_a.push(curr_['NDF'] * percent_val)
+
+
+                dm_a.push(curr_['DM%'] * percent_val * curr_['DM%'] / 100)
+                cp_a.push(curr_['CP'] * percent_val * curr_['DM%'] / 100)
+                me_a.push(curr_['ME'] * percent_val * curr_['DM%'] / 100)
+                ndf_a.push(curr_['NDF'] * percent_val * curr_['DM%'] / 100)
+
+
+                // dm_a.push(curr_['DM%'] * percent_val)
+                // cp_a.push(curr_['CP'] * percent_val)
+                // me_a.push(curr_['ME'] * percent_val)
+                // ndf_a.push(curr_['NDF'] * percent_val)
+
+                console.log(String(y) + ". " + JSON.stringify(curr_) + String(percent_val))
+
+                console.log(
+                  curr_['DM%'] * percent_val,
+                  curr_['CP'] * percent_val,
+                  curr_['ME'] * percent_val,
+                  curr_['NDF'] * percent_val
+                )
 
                 return (
                   <View key={k} >
@@ -100,27 +115,30 @@ const ResultCheck = (props) => {
                         <Text style={{ fontSize: 24, fontWeight: '650' }}>{percent_val} %</Text>
                       </View>
                     </View>
-                    <Text>
+                    <Text style={{ backgroundColor: 'red' }}>
                       DM%: {curr_['DM%']}
                       CP: {curr_['CP']}
                       ME: {curr_['ME']}
                       NDF: {curr_['NDF']}
                     </Text>
-                    <Text>
+                    <Text style={{ backgroundColor: 'green' }}>
                       Provides
-                      Dry Matter {curr_['DM%'] * percent_val} {dm_a.push(curr_['DM%'] * percent_val)}
-                      CP {curr_['CP'] * percent_val} {cp_a.push(curr_['CP'] * percent_val)}
-                      ME {curr_['ME'] * percent_val} {me_a.push(curr_['ME'] * percent_val)}
-                      NDF {curr_['NDF'] * percent_val} {ndf_a.push(curr_['NDF'] * percent_val)}
+                      Dry Matter {curr_['DM%'] * percent_val}
+                      {/* {dm_a.push(curr_['DM%'] * percent_val)} */}
+                      CP {curr_['CP'] * percent_val}
+                      {/* {cp_a.push(curr_['CP'] * percent_val)} */}
+                      ME {curr_['ME'] * percent_val}
+                      {/* {me_a.push(curr_['ME'] * percent_val)} */}
+                      NDF {curr_['NDF'] * percent_val}
+                      {/* {ndf_a.push(curr_['NDF'] * percent_val)} */}
                     </Text>
-
                   </View>
                 )
               })
             }
           </View>
 
-          {/* this formula contains */}
+          {/* this formula contains / nutrient composition of feed */}
           <View style={{
             backgroundColor: "rgb(30, 130, 30)", borderRadius: 10, padding: 10, marginTop: 20
           }}>
@@ -133,7 +151,7 @@ const ResultCheck = (props) => {
                 }, 0)) / 100).toFixed(2)
               }
             </Text>
-            <Text style={{ color: "#fff", fontSize: 22, fontWeight: '600', alignSelf: "center" }}>
+            <Text style={{ color: "#fff", fontSize: 22, fontWeight: '600', alignSelf: "center", backgroundColor: 'blue' }}>
               {t("CP")}:
               {
                 ((cp_a.reduce(function (x, y) {
@@ -174,39 +192,39 @@ const ResultCheck = (props) => {
               Object.keys(res['results']).map(k => {
                 curr_ = nutrientdata.find(x => x.name == k)
                 percent_val = Math.round(res['results'][k] * 100)
-                dm_a.push(curr_['DM%'] * percent_val)
-                cp_a.push(curr_['CP'] * percent_val)
-                me_a.push(curr_['ME'] * percent_val)
+                // dm_a.push(curr_['DM%'] * percent_val)
+                // cp_a.push(curr_['CP'] * percent_val)
+                // me_a.push(curr_['ME'] * percent_val)
 
                 return (
                   <View key={k} >
                     <View style={{
                       flex: 1, flexDirection: 'row',
-                      // justifyContent: "flex-start",//'space-between', 
                       justifyContent: 'space-between',
                       borderBottomColor: 'rgb(120, 30, 0)', borderBottomWidth: 1
                     }}>
                       <View style={{ height: 25 }}>
                         <Text style={{
-                          fontSize: 18, fontWeight: 'bold', width: 200 //</View>borderRightWidth: 2, borderRightColor: 'black', width: 100 
+                          fontSize: 18, fontWeight: 'bold', width: 100
                         }}>
-                          {/* {k} */}
                           {t(k)}
                         </Text>
                       </View>
-                      {/* comeented below */}
+
                       {/* <View style={{ height: 25 }}>
-                        <Text style={{ fontSize: 18, fontWeight: 'bold', borderRightWidth: 2, borderRightColor: 'black', width: 40 }}>{percent_val} %</Text>
-                      </View>
-                      <View style={{ height: 25 }}>
-                        <Text style={{ fontSize: 18, fontWeight: 'bold', borderRightWidth: 2, borderRightColor: 'black', width: 70 }}>{percent_val * dmi_req / 100} %</Text>
-                      </View> */}
-                      <View style={{ height: 25 }}>
                         <Text style={{
                           fontSize: 18, fontWeight: 'bold', //borderRightWidth: 2, borderRightColor: 'black', width: 70 
                         }}>
                           {
                             ((percent_val * dmi_req / 100) / (curr_['DM%'] / 100)).toFixed(2)
+                          } Kg
+                        </Text>
+                      </View> */}
+
+                      <View style={{ height: 25 }}>
+                        <Text style={{ fontSize: 18, fontWeight: 'bold' }}>
+                          {
+                            ((percent_val * 100) / (curr_['DM%'] * dmi_req)).toFixed(2)
                           } Kg
                         </Text>
                       </View>
@@ -216,29 +234,26 @@ const ResultCheck = (props) => {
               })
             }
           </View>
-
-
         </View>
       )
     }
+    // if the correct least cost result is not available
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: "center" }}>
-        {/* <Text>Available but incorrect results</Text> */}
         <Image
-          style={{ width: 35, height: 35, flex: 1, justifyContent: 'center', alignItems: "center" }}
+          style={{ width: '100%', height: 300, flex: 1, justifyContent: 'center', alignItems: "center" }}
           source={require('../assets/images/warn.gif')}
         />
-
         <Text style={{ fontSize: 18, fontWeight: '500' }}>{status_[res['status']]}</Text>
       </View>
     )
   }
+  // unexpected errors like no connectivity or damaged app
   else {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: "center" }}>
-        {/* <Text>N/A component</Text> */}
         <Image
-          style={{ width: 35, height: 35, flex: 1, justifyContent: 'center', alignItems: "center" }}
+          style={{ width: '100%', height: 300, flex: 1, justifyContent: 'center', alignItems: "center" }}
           source={require('../assets/images/important.gif')}
         />
         <Text style={{ fontSize: 18, fontWeight: '500' }}>An unexpected error happened!</Text>
@@ -254,9 +269,16 @@ function DetailsScreen({ navigation, route }) {
   const [isLoading, setLoading] = useState(true);
   const [data, setData] = useState([]);
   const [nutReq, setNutReq] = useState([])
+  const [nutReqShow, setNutReqShow] = useState([])
   const [compo, setCompo] = useState([]);
   const [bwt, setbwt] = useState(0)
   const [calDMI, setCalDMI] = useState(0)
+
+  const windowWidth = Dimensions.get('window').width;
+  const windowHeight = Dimensions.get('window').height;
+
+  console.log("Device Dimensions: ")
+  console.log(windowWidth, windowHeight)
 
   const { t } = useTranslation();
 
@@ -350,6 +372,7 @@ function DetailsScreen({ navigation, route }) {
     // console.log(cp_T)
     // console.log(me_T)
     setCalDMI(dmi)
+    // change following conversions
     let me = (Number(me_T) / Number(dmi)).toFixed(2)
     console.log('cp')
     console.log(cp)
@@ -362,6 +385,17 @@ function DetailsScreen({ navigation, route }) {
     // console.log(found)
     // console.log("found")
     setNutReq([Number(cp), Number(me), 28])
+    // to these conversions
+    // let me = (Number(me_T) / Number(dmi)).toFixed(2)
+    // console.log('cp')
+    // console.log(cp)
+    // newlist.push(cp)
+    // let cp = (Number(cp_T) / (Number(dmi) * 1000) * 100).toFixed(2)
+    // console.log('me')
+    // console.log(me)
+    // Changing this on March 30
+    setNutReqShow([Number(cp_T), Number(me_T), 28])
+
   }
 
   useEffect(() => {
@@ -391,16 +425,41 @@ function DetailsScreen({ navigation, route }) {
         }
       </View>
 
-      {/* <View style={{ flex: 1, justifyContent: 'flex-end', flexDirection: "row", alignSelf: 'center', alignItems: 'center', marginBottom: 10, marginTop: 10 }}>
-        <Image
-          style={{ width: 100, height: 45 }}
-          source={require('../assets/uvas-big.png')}
-        />
-        <Image
-          style={{ width: 100, height: 45 }}
-          source={require('../assets/arass.png')}
-        />
-      </View> */}
+      {/* animal requirements */}
+      <View style={{
+        backgroundColor: "rgba(10, 120, 10, 0.9)", borderRadius: 10, padding: 10, marginVertical: 20, marginBottom: 30
+      }}>
+        <Text style={{ fontSize: 24, color: "#fff", fontWeight: 'bold', alignSelf: "center" }}>
+          {t("animal requires line")}
+        </Text>
+        <Text style={{ color: "#fff", fontSize: 20, fontWeight: '600', alignSelf: "center" }}>
+          {t("CP")}: {nutReqShow[0]} Grams
+        </Text>
+        <Text style={{ color: "#fff", fontSize: 20, fontWeight: '600', alignSelf: "center" }}>
+          {t("ME")}: {nutReqShow[1]} kcal
+        </Text>
+        <Text style={{ color: "#fff", fontSize: 20, fontWeight: '600', alignSelf: "center" }}>
+          {t("NDF")}: {(nutReqShow[2] / 100) * calDMI} Kilos
+        </Text>
+        <Text style={{ color: "#fff", fontSize: 20, fontWeight: '600', alignSelf: "center" }}>
+          {t("Body Weight")}: {bwt} KG
+        </Text>
+        <Text style={{ color: "#fff", fontSize: 20, fontWeight: '600', alignSelf: "center" }}>
+          {t("Dry Matter Intake")}: {calDMI} KG
+        </Text>
+
+        {/* <Text style={{ color: "#fff", fontSize: 20, fontWeight: '600', alignSelf: "center" }}>
+          {t("Dry Matter Intake")}: {JSON.stringify(nutReq)}
+        </Text> */}
+        {/* <Text style={{ color: "#fff", fontSize: 20, fontWeight: '600', alignSelf: "center" }}>
+          {t("Dry Matter Intake")}: {(bwt * 2 / 100)}
+        </Text>
+        <Text>
+          {t("Dry Matter Intake")}: {calDMI}
+        </Text> */}
+      </View>
+
+      {/* sponsors display */}
       <View style={{
         flex: 1, width: '100%', flexDirection: 'row', alignItems: "center", justifyContent: 'space-around',
         borderColor: "black", borderWidth: 1, backgroundColor: 'rgb(10, 100, 10)', height: 50
@@ -412,7 +471,6 @@ function DetailsScreen({ navigation, route }) {
           }}>
             Our Partners
           </Text>
-
         </View>
 
         <TouchableOpacity
@@ -436,33 +494,7 @@ function DetailsScreen({ navigation, route }) {
             />
           </View>
         </TouchableOpacity>
-
       </View>
-
-      {/* <ResultCheck result={data} compo={compo} navigate={navigation} /> */}
-
-      {/* <View style={{
-        backgroundColor: "rgb(30, 130, 30)", borderRadius: 10, padding: 10, marginVertical: 20, marginBottom: 30
-      }}>
-        <Text style={{ fontSize: 24, color: "#fff", fontWeight: 'bold', alignSelf: "center" }}>
-          {t("animal requires line")}
-        </Text>
-        <Text style={{ color: "#fff", fontSize: 20, fontWeight: '600', alignSelf: "center" }}>
-          {t("CP")}: {nutReq[0]} %
-        </Text>
-        <Text style={{ color: "#fff", fontSize: 20, fontWeight: '600', alignSelf: "center" }}>
-          {t("ME")}: {nutReq[1]}
-        </Text>
-        <Text style={{ color: "#fff", fontSize: 20, fontWeight: '600', alignSelf: "center" }}>
-          {t("Body Weight")}: {bwt}
-        </Text>
-        <Text style={{ color: "#fff", fontSize: 20, fontWeight: '600', alignSelf: "center" }}>
-          {t("Dry Matter Intake")}: {(bwt * 2 / 100)}
-        </Text>
-        <Text>
-          {t("Dry Matter Intake")}: {calDMI}
-        </Text>
-      </View> */}
 
     </ScrollView>
   );
